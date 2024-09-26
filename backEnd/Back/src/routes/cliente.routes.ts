64a3fb9b044aba35ec738504c4f040f85429f0ea
Bruno <brucascardo@gmail.com>
2024-09-26 15:05:00 -0300

    Cambia carpetas y el package.json

--- backEnd/Back/src/routes/cliente.routes.ts
+++
@@ -1,8 +0,0 @@
-import { findAll, findOne } from "../controllers/cliente.controller.js";
-import { Router } from "express";
-
-export const routerCliente = Router();
-
-routerCliente.get('/', findAll);
-
-routerCliente.get('/:id', findOne);
