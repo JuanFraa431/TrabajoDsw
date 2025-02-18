8f13b935b9169cc6c56bfb7c6477d007199b948f
BruCascardo <brucascardo@gmail.com>
2025-02-18 17:01:15 -0300

    Cambia nullable persona

--- Back/src/models/persona.model.ts
+++ Back/src/models/persona.model.ts
@@ -1,21 +1,21 @@
 import { BaseModel } from '../shared/db/baseModel.model.js';
-import { Entity, OneToMany, Property } from '@mikro-orm/core';
+import { Entity, Property } from '@mikro-orm/core';
 
 @Entity()
 export class Persona extends BaseModel {
-    @Property({ nullable: false })
+    @Property({ nullable: true })
     nombre!: string;
 
-    @Property({ nullable: false })
+    @Property({ nullable: true })
     apellido!: string;
 
-    @Property({ nullable: false })
+    @Property({ nullable: true })
     dni!: string;
 
-    @Property({ nullable: false })
+    @Property({ nullable: true })
     email!: string;
 
-    @Property({ nullable: false })
+    @Property({ nullable: true })
     fecha_nacimiento!: Date;
 
 }
