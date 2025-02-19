d55f6950e6a38725bac6ce1fdf6f1069987d0ad5
JuanFraa431 <juanfraa032@gmail.com>
2025-02-19 15:08:14 -0300

    fix

--- Front/src/components/Cliente/EditarPerfil.tsx
+++ Front/src/components/Cliente/EditarPerfil.tsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { Cliente } from '../../interface/cliente';
 import '../../styles/Cliente/EditarPerfil.css';
 
 const EditarPerfil: React.FC = () => {
-    const location = useLocation();
     const navigate = useNavigate();
     const cliente = JSON.parse(localStorage.getItem('user') || '{}')
 
